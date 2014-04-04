8ea992f451c3c38690e7be7b4562418336667e16
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-04-04 09:54:57 -0700

    modified install script

--- hadoop_install.py
+++ hadoop_install.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
-# hadoop install script for mac osx
+#
+# this is a hadoop install script for mac osx
+# IMPORTANT: Make sure that you turn on remote login under system preferences then file sharing
+#
+
 import os
 import re
 import sys
@@ -9,14 +13,18 @@ import commands
 hdfs_path = "/Users/geraldstanje/Documents/" # dont forget the / at the end
 hdfs_dir = "hdfstmp"
 
+#
 # install hadoop
+#
 os.system("install hadoop...")
 os.system("sleep 2")
 
 # install hadoop using brew
 os.system("brew install hadoop")
 
+#
 # set env vars for hadoop
+#
 array = []
 env_vars_find = ["export JAVA_HOME", "export HADOOP_HOME", "export PATH=$PATH:$HADOOP_INSTALL/bin\n", "export PATH=$PATH:$HADOOP_INSTALL/sbin\n"]
 env_vars_repl = ["export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home\n", "export HADOOP_HOME=/usr/local/Cellar/hadoop/2.3.0\n", "export PATH=$PATH:$HADOOP_INSTALL/bin\n", "export PATH=$PATH:$HADOOP_INSTALL/sbin\n"]
@@ -61,28 +69,36 @@ fh.close()
 # refresh env vars in current shell
 os.system("source " + home_dir + "/.profile")
 
+#
 # create hdfs dir
+#
 os.system("mkdir -p" + hdfs_path + hdfs_dir)
 
+#
 # copy the property files (stored in config) to /usr/local/Cellar/hadoop/2.3.0/libexec/etc/hadoop
-# copy core-site.xml
-os.system("cp ./config/core-site.xml $HADOOP_HOME/libexec/etc/hadoop")
-# copy yarn-site.xml
-os.system("cp ./config/yarn-site.xml $HADOOP_HOME/libexec/etc/hadoop")
-# copy mapred-site.xml
-os.system("cp ./config/mapred-site.xml $HADOOP_HOME/libexec/etc/hadoop")
-# copy hdfs-site.xml
-os.system("cp ./config/hdfs-site.xml $HADOOP_HOME/libexec/etc/hadoop")
+#
+os.system("cp ./config/core-site.xml $HADOOP_HOME/libexec/etc/hadoop") # copy core-site.xml from config folder to hadoop path
+os.system("cp ./config/yarn-site.xml $HADOOP_HOME/libexec/etc/hadoop") # copy yarn-site.xml from config folder to hadoop path
+os.system("cp ./config/mapred-site.xml $HADOOP_HOME/libexec/etc/hadoop") # copy mapred-site.xml from config folder to hadoop path
+os.system("cp ./config/hdfs-site.xml $HADOOP_HOME/libexec/etc/hadoop") # copy hdfs-site.xml from config folder to hadoop path
 
-# create two directories which will contain the namenode and the datanode for this Hadoop installation
+#
+# create two directories which will contain the namenode and the datanode for this hadoop installation
+#
 os.system("mkdir -p " + hdfs_path + hdfs_dir + "/namenode")
 os.system("mkdir -p " + hdfs_path + hdfs_dir + "/datanode")
 
-# format the New Hadoop Filesystem
+#
+# format the new hadoop filesystem
+#
 os.system("hdfs namenode -format")
 
+#
 # start hadoop
+#
 os.system("$HADOOP_HOME/sbin/start-dfs.sh")
 
+#
 # show functional instance of Hadoop running on your VPS
+#
 os.system("jps")
